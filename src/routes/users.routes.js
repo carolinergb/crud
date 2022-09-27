54c5f3007a85534e0a4ae8373d1706d809499731
Caroline <carolineamrb@gmail.com>
2022-09-27 06:01:27 -0300

    feat: adicionado middlewares em algumas rotas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,11 +6,13 @@ import {
 	retrieveUserController,
 	updateUserController,
 } from "../controllers/users.controller";
+import { checkEmail } from "../middlewares/checkEmail.middleware";
+import { userIsAdmin } from "../middlewares/userIsAdmin.middleware";
 
 export const userRoutes = Router();
 
-userRoutes.post("", createUserController);
-userRoutes.get("", listUserController);
+userRoutes.post("", checkEmail, createUserController);
+userRoutes.get("", userIsAdmin, listUserController);
 userRoutes.get("/:id", retrieveUserController);
 userRoutes.delete("/:id", deleteUserController);
 userRoutes.patch("/:id", updateUserController);
