b8ec448c525ac066dd2537a72aaaa6ec3602c7fe
Caroline <carolineamrb@gmail.com>
2022-09-27 06:03:25 -0300

    feat: criei um novo objeto de usuário no formato que foi solicitado

--- src/services/users/createUser.service.js
+++ src/services/users/createUser.service.js
@@ -1,8 +1,17 @@
 import { users } from "../../database";
 import { v4 as uuidv4 } from "uuid";
 
-export const createUserService = (user) => {
-	user.id = uuidv4();
-	users.push(user);
-	return user;
+export const createUserService = (name, email, isAdm) => {
+	const newUser = {
+		id: uuidv4(),
+		name,
+		email,
+		isAdm,
+		createdOn: new Date(),
+		updatedOn: new Date(),
+	};
+
+	users.push(newUser);
+
+	return newUser;
 };
